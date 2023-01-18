df15c66fddc82b556a134c57d9941c6575b1ee29
ittp <61617660+ittp@users.noreply.github.com>
2023-01-18 11:55:09 +0300

    1 file modified, 1 added

---
+++ utils/converter.js
@@ -0,0 +1,14 @@
+// require csvtojson module
+const CSVToJSON = require('csvtojson');
+
+// convert users.csv file to JSON array
+(async () => {
+  try {
+    const users = await CSVToJSON().fromFile('users.csv');
+
+    // log the JSON array
+    console.log(users);
+  } catch (err) {
+    console.log(err);
+  }
+})();

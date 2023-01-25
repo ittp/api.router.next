8964dd7d349317bdad4f76f4eb2e4e28cec4f24b
ittp <61617660+ittp@users.noreply.github.com>
2023-01-26 01:14:25 +0300

    2 files modified

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -3,6 +3,7 @@ import type { User } from '../../interfaces'
 
 // Fake users data
 const users: User[] = [{ id: 1 }, { id: 2 }, { id: 3 }]
+// const rooms: User[] = [{ id: 1 }, { id: 2 }, { id: 3 }]
 
 export default function handler(
   _req: NextApiRequest,

d46c5dd3a6df2a6db90afadaf4387ec6f1edde5b
ittp <61617660+ittp@users.noreply.github.com>
2023-02-05 01:33:15 +0300

    1 file modified

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -2,10 +2,15 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import type { User } from '../../interfaces';
 
 // Fake users data
-let a = new Range(0, 1000);
+let N = 1000;
+let a1 = Array.apply(null, { length: N }).map(Number.call, (i, k) => {
+  console.log(i);
 
-console.log(a);
-const users: User[] = [{ id: 1 }, { id: 2 }, { id: 3 }];
+  return { id: i + 1 };
+});
+
+const users: User[] = a1;
+
 // const rooms: User[] = [{ id: 1 }, { id: 2 }, { id: 3 }]
 
 export default function handler(
@@ -13,7 +18,7 @@ export default function handler(
   res: NextApiResponse<User[]>
 ) {
   // Get data from your database
+
+  console.log(users);
   res.status(200).json(users);
 }
-
-

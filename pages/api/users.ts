043b1eb411117ef3867a1b16137056294766ac2c
ittp <61617660+ittp@users.noreply.github.com>
2023-02-05 01:27:31 +0300

    1 file modified

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,10 +1,11 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import type { User } from '../../interfaces'
-
-
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { User } from '../../interfaces';
 
 // Fake users data
-const users: User[] = [{ id: 1 }, { id: 2 }, { id: 3 }]
+let a = new Range(0, 1000);
+
+console.log(a);
+const users: User[] = [{ id: 1 }, { id: 2 }, { id: 3 }];
 // const rooms: User[] = [{ id: 1 }, { id: 2 }, { id: 3 }]
 
 export default function handler(
@@ -12,5 +13,7 @@ export default function handler(
   res: NextApiResponse<User[]>
 ) {
   // Get data from your database
-  res.status(200).json(users)
+  res.status(200).json(users);
 }
+
+
